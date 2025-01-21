089c74d24f74ef5e332cd4913784e3b60cea1eaa
subbunv86 <subbunv86@gmail.com>
2025-01-21 21:54:48 +0530

    Send_Email

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests as req
+from send_email import send_email
 import json as js
+apiKey = '46f202589db44e01a664edafeea480f0'
 url = "https://newsapi.org/v2/everything?q=Tesla&sortBy=publishedAt&apiKey=46f202589db44e01a664edafeea480f0"
 
 request = req.get(url)
 content = request.json()
-
+body = ''
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    body += article['title'] + '\n' + article['description'] + '\n\n'
+body = body.encode('utf-8')    
+send_email(body)
---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib,ssl
+import os
+host="smtp.gmail.com"
+port=465
+username="subbu.venkat86@gmail.com"
+paswword="eoux uydp pwzb pmhp"
+
+receiver_email="subbunv86@gmail.com"
+context = ssl.create_default_context()
+
+
+def send_email(Message):
+    
+
+    with smtplib.SMTP_SSL(host,port,context=context) as server:
+        server.login(username,paswword)
+        server.sendmail(username,receiver_email,Message)
